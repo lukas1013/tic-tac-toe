1f5696520f40d659ca32054a69df4d945122699b
lukas1013 <leonardolukas10@gmail.com>
2021-10-11 20:49:39 -0300

    server: adds typescript

---
+++ packages/server/src/app.ts
@@ -0,0 +1,11 @@
+import { router } from "./routes"
+import express from 'express'
+// import cors from 'cors'
+
+const app = express()
+
+// app.use(cors())
+app.use(express.json())
+app.use(router)
+
+export { app }
